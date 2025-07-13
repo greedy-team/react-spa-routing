4abd52734f2fff11f930a4fc94ae4fa8881e7482
gxuoo <rbdud189@naver.com>
2025-07-13 23:44:21 +0900

    refactor: URL 소문자 변경 및 Layout 설정

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Header from "../components/head/Header";
 import NewsContainer from "../components/main/NewsConatainer";
+import MainLayout from "../components/layout/MainLayout";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
-        <Route path="/" element={<Navigate to="/All" />} />
-        <Route path="/:category" element={<NewsContainer />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Navigate to="/all" />} />
+          <Route path="/:category" element={<NewsContainer />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
