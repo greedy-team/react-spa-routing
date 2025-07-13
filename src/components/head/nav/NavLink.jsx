2d4cf9b62d3d42d060eff5785908a6af650ccf5b
gxuoo <rbdud189@naver.com>
2025-07-13 23:45:39 +0900

    refactor: NavLink 태그 변경

--- src/components/head/nav/NavLink.jsx
+++ src/components/head/nav/NavLink.jsx
@@ -1,20 +1,21 @@
-import { useLocation } from "react-router-dom";
+import { NavLink as RouterNavLink } from "react-router-dom";
 
 function NavLink({ href, children }) {
-  const location = useLocation();
-  const isActive = location.pathname === href;
-
   return (
-    <a
-      href={href}
-      className={`transition-transform duration-200 ease-in-out inline-block ${
-        isActive
-          ? "text-cyan-300 dark:text-cyan-400 text-2xl scale-[1.05] underline decoration-4 underline-offset-8"
-          : "text-white dark:text-gray-400 text-2xl hover:scale-[1.05] hover:text-cyan-300 dark:hover:text-cyan-400 hover:underline hover:decoration-4 hover:underline-offset-8"
-      }`}
+    <RouterNavLink
+      to={href}
+      className={({ isActive }) => `
+        transition-transform duration-200 ease-in-out inline-block
+        ${
+          isActive
+            ? "text-cyan-300 dark:text-cyan-400 text-2xl scale-[1.05] underline decoration-4 underline-offset-8"
+            : "text-white dark:text-gray-400 text-2xl hover:scale-[1.05] hover:text-cyan-300 dark:hover:text-cyan-400 hover:underline hover:decoration-4 hover:underline-offset-8"
+        }
+      `}
+      end={true}
     >
       {children}
-    </a>
+    </RouterNavLink>
   );
 }
 
