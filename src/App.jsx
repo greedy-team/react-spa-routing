a0c5813a9b357acfbf78ed84640722bea00e0de2
yoonjaehong26 <yjh0202261@gmail.com>
2026-01-09 00:02:03 +0900

    feat:axios로 기사 데이터 가져오는 기능추가 및 .env추적제거
    NewsAPI.org api구조에 따라 앤드포인트에 everything 사용 시 검색어가 필요하고
    top-headlines사용 시 카테고리나 국가가 필요하기에 임시로 everything과 test검색어 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,51 @@
-import './App.css';
+import { useState, useEffect } from 'react';
+import Card from './main/newsCard.jsx';
+import axios from 'axios';
 
 function App() {
+  const [fetchdedArticles, setFetchdedArticles] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchedData = async () => {
+      setIsLoading(true);
+      try {
+        const apiKey = import.meta.env.VITE_NEWS_API_KEY;
+        const searchKeyword = 'test';
+
+        const response = await axios.get(
+          `https://newsapi.org/v2/everything?q=${searchKeyword}&apiKey=${apiKey}`,
+        );
+
+        console.log('전체 응답 데이터:', response.data);
+        setFetchdedArticles(response.data.articles);
+
+      } catch (error) {
+        console.error('에러 발생:', error);
+      }
+      setIsLoading(false);
+    };
+
+    fetchedData();
+  }, []);
+
+  if (isLoading) {
+    return <div>뉴스를 불러오는 중입니다...</div>;
+  }
+
+  if (!fetchdedArticles) {
+    return <div>뉴스가 없습니다.</div>;
+  }
 
   return (
     <>
-      <h1>그리디 미션 : 뉴스 웹 뷰어 시이작!</h1>
+      <header></header>
 
+      <main>
+        <Card articles={fetchdedArticles} />
+      </main>
+
+      <footer></footer>
     </>
   );
 }
